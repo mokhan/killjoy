3a0b09d2248d33d42994854a90073926efe93fbc
mo khan <mo@mokhan.ca>
2015-10-17 13:48:01 -0600

    add option to run workers using futures api.

--- lib/killjoy/consumer.rb
+++ lib/killjoy/consumer.rb
@@ -3,11 +3,34 @@ module Killjoy
     include Sneakers::Worker
     from_queue "sharding: shard.killjoy - rabbit@localhost - #{ENV.fetch("RMQ_SHARD", "1")}"
 
-    def work(message)
-      log ["session:", session.object_id].inspect
-      batch = batch_for(JSON.parse(message, symbolize_names: true))
-      session.execute(batch)
-      ack!
+    if ENV["ASYNC"] == "true"
+      def work_with_params(message, delivery_info, metadata)
+        log ["ASYNC session:", session.object_id].inspect
+
+        tag_closure = delivery_info.delivery_tag
+        batch = batch_for(JSON.parse(message, symbolize_names: true))
+        future = session.execute_async(batch)
+
+        future.on_success do |values|
+          log ["SUCCESS", values]
+          channel.acknowledge(tag_closure, false)
+        end
+        future.on_failure do |error|
+          log ["FAIL", error]
+          channel.reject(tag_closure, false)
+        end
+      end
+
+      def channel
+        @channel ||= @queue.instance_variable_get("@channel")
+      end
+    else
+      def work(message)
+        log ["session:", session.object_id].inspect
+        batch = batch_for(JSON.parse(message, symbolize_names: true))
+        session.execute(batch)
+        ack!
+      end
     end
 
     def session
