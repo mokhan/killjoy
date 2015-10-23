986edae879ae55aac5fd31adce3cf2eccdaecb63
mo khan <mo@mokhan.ca>
2015-10-22 23:04:24 -0600

    make async worker inherit from sync worker.

--- lib/killjoy/async_consumer.rb
+++ lib/killjoy/async_consumer.rb
@@ -1,25 +1,12 @@
+require "killjoy/consumer"
+
 module Killjoy
-  class AsyncConsumer
-    attr_reader :shard, :writers
-
-    def initialize(writers, shard)
-      @shard = shard
-      @writers = writers
-    end
-
+  class AsyncConsumer < Killjoy::Consumer
     def work(message)
       writes = writers.map do |writer|
         writer.write(message.to_hash, async: true)
       end
       message.process(::Cassandra::Future.all(writes))
-    end
-
-    def bindings
-      [queue_name]
-    end
-
-    def queue_name
-      "sharding: killjoy - rabbit@localhost - #{shard}"
     end
   end
 end
