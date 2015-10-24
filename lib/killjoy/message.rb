e9cc5885526872b34ddae96816f10d69dae4deaa
mo khan <mo@mokhan.ca>
2015-10-24 10:56:11 -0600

    improve test script to wait for async writes to complete.

--- lib/killjoy/message.rb
+++ lib/killjoy/message.rb
@@ -6,6 +6,11 @@ module Killjoy
       @to_hash = JSON.parse(raw_message, symbolize_names: true)
       @info = info
       @channel = channel
+      @interceptors = { ack: [], reject: [] }
+    end
+
+    def intercept(response_type, &block)
+      @interceptors[response_type] << block
     end
 
     def process(future)
@@ -18,11 +23,21 @@ module Killjoy
     end
 
     def ack!
+      run_interceptors_for(:ack)
       channel.acknowledge(info.delivery_tag, false)
     end
 
     def reject!(requeue = false)
+      run_interceptors_for(:reject)
       channel.reject(info.delivery_tag, requeue)
+    end
+
+    private
+
+    def run_interceptors_for(response_type)
+      @interceptors[response_type].each do |interceptor|
+        interceptor.call
+      end
     end
   end
 end
